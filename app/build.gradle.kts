e65925acf2b4a9562ce3db4631537fd8dd44e395
graysomb <micharlg2767@gmail.com>
2024-05-05 20:58:30 -0600

    implemented export function

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -93,5 +93,7 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
 
+    implementation("com.opencsv:opencsv:5.7.1")
+
 
 }
